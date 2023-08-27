8b0868c3711178d2c92becca4dcf25d891fa67ed
damianludwiczak89 <103378303+damianludwiczak89@users.noreply.github.com>
2023-08-27 20:11:39 +0200

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -17,6 +17,8 @@ class Solution(object):
 
 1. Two Sum
 
+https://leetcode.com/problems/two-sum/
+
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
